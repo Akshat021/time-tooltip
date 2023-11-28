9bb6fc80e0dacb035ac8f5dc8469d8c3be8949ef
Akshat <akshatvyas958@gmail.com>
2023-11-28 12:14:18 +0530

    future time support

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ const App = () => {
   const style = {
     "color" : "red"
   }
-
+  
+  // const sevenDaysAgo = new Date(1701150663732);
+  const sevenDaysAgo = new Date();
+  // sevenDaysAgo.setDate(sevenDaysAgo.getDate());
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate()-1);
   return (
       <Box>
         {/* <Tooltip title={"Drag me!"} className='dragHandle'> */}
-          <Time style={style} date={new Date()} />
+          <Time style={style} date={sevenDaysAgo } />
         {/* </Tooltip> */}
-        
+          {console.log(new Date())}
       </Box>
   )
 }
--- src/lib/components/TimeTooltip.js
+++ src/lib/components/TimeTooltip.js
@@ -1,49 +1,75 @@
 import { Tooltip } from "@mui/material";
 import React from "react";
 
-const Time = ({ date, style }) => {
-
-    const formatDateTime = (dateTime) => {
-        const currentDate = new Date();
-        let createdDate;
-        if(typeof dateTime === 'object' && dateTime instanceof Date) createdDate = dateTime;
-        else createdDate = new Date(dateTime);
-        const diff = currentDate - createdDate;
-        if (diff < 60 * 1000) {
-            const seconds = Math.floor(diff / 1000);
-            return `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
-        } else if (diff < 60 * 60 * 1000) {
-            const minutes = Math.floor(diff / (60 * 1000));
-            const seconds = Math.floor((diff % (60 * 1000)) / 1000);
-            return `${minutes} minute${minutes !== 1 ? "s" : ""} and ${seconds} second${seconds !== 1 ? "s" : ""} ago`;
-        } else if (diff < 24 * 60 * 60 * 1000) {
-            const hours = Math.floor(diff / (60 * 60 * 1000));
-            return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
-        } else if (diff < 7 * 24 * 60 * 60 * 1000) {
-            const days = Math.floor(diff / (24 * 60 * 60 * 1000));
-            if (days === 1) {
-                return "Yesterday";
-            } else {
-                const formattedDate = createdDate.toLocaleString("en-US", { day: "numeric", month: "long" });
-                return formattedDate;
-            }
-        } else if (diff < 12 * 30 * 24 * 60 * 60 * 1000) {
-            const months = Math.floor(diff / (30 * 24 * 60 * 60 * 1000));
-            return `${months} month${months !== 1 ? "s" : ""} ago`;
-        } else if (diff < 365 * 24 * 60 * 60 * 1000) {
-            const year = createdDate.getFullYear();
-            const month = createdDate.toLocaleString("en-US", { month: "long" });
-            return `${month} ${year}`;
+const Time = ({ date }) => {
+  const formatDateTime = (dateTime) => {
+    const currentDate = new Date();
+    const createdDate = new Date(dateTime);
+    let timeDifference = currentDate - createdDate;
+    let isFuture = false;
+    console.log(timeDifference)
+    if(timeDifference < 0){
+      isFuture = true;
+      timeDifference *= -1; 
+    }
+    if (timeDifference === 1000) {
+      return "Just now";
+    }
+    else if (timeDifference < 60 * 1000) {
+      const seconds = Math.floor(timeDifference / 1000);
+      return `${isFuture ? 'In ' : ''}${seconds} second${seconds !== 1 ? "s" : ""}${isFuture ? '' : ' ago'}`;
+    }
+    else if (timeDifference < 60 * 60 * 1000) {
+      const minutes = Math.floor(timeDifference / (60 * 1000));
+      const seconds = Math.floor((timeDifference % (60 * 1000)) / 1000);
+      return `${isFuture ? 'In ' : ''}${minutes} minute${minutes !== 1 ? "s" : ""} and ${seconds} second${seconds !== 1 ? "s" : ""}${isFuture ? '' : ' ago'}`;
+    }
+    else if (timeDifference < 24 * 60 * 60 * 1000) {
+      const hours = Math.floor(timeDifference / (60 * 60 * 1000));
+      return `${isFuture ? 'In ' : ''}${hours} hour${hours !== 1 ? "s" : ""}${isFuture ? '' : ' ago'}`;
+    }
+    else if (timeDifference < 7 * 24 * 60 * 60 * 1000) {
+      const days = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+      if (days === 1) {
+        return isFuture ? "Tomorrow" : "Yesterday";
+      } else {
+        const sameYear = currentDate.getFullYear() === createdDate.getFullYear();
+        const monthName = createdDate.toLocaleString("default", { month: "long" });
+        if (sameYear) {
+          const date = createdDate.getDate();
+          return `${date} ${monthName}`;
         } else {
-            const years = Math.floor(diff / (365 * 24 * 60 * 60 * 1000));
-            return `${years} year${years !== 1 ? "s" : ""} ago`;
+          const year = createdDate.getFullYear();
+          return `${monthName} ${year}`;
         }
-    };
-  
-    return (
-        <Tooltip sx={{color : "blue"}} key={date.getTime()} title={JSON.stringify(date.toLocaleString())} placement="top">
-            <span  key={date}>{formatDateTime(date)}</span>
-        </Tooltip>
-    )
+      }
+    }
+    else if (timeDifference < 12 * 30 * 24 * 60 * 60 * 1000) {
+      const weeks = Math.floor(timeDifference / (7 * 24 * 60 * 60 * 1000));
+      return `${isFuture ? 'In ' : ''}${weeks} week${weeks !== 1 ? "s" : ""}${isFuture ? '' : ' ago'}`;
+    }
+
+    const year = createdDate.getFullYear();
+    const monthName = createdDate.toLocaleString("default", { month: "long" });
+    return `${monthName} ${year}`;
+  };
+
+  return (
+    // <Tooltip sx={{color : "blue"}} key={date.getTime()} title={JSON.stringify(date.toLocaleString())} placement="top">
+    <Tooltip
+      title={new Date(date).toLocaleString(undefined, {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false // This ensures 24-hour format
+      })}
+      placement="top"
+    >
+      <span key={date}>{formatDateTime(date)}</span>
+    </Tooltip>
+  )
 }
 export default Time;
